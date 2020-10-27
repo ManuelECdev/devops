2494b0f672e636888236ff33e096db2c1a379eda
Manuel Eiras Casal <manuel.eirascasal@icloud.com>
2020-10-27 20:28:21 +0000

    adding docker build

--- build_docker.sh
+++ build_docker.sh
@@ -5,7 +5,7 @@
 # Step 1:
 # Build image and add a descriptive tag
 echo "building with docker"
-docker build --tag=udafinalproject-${env.BUILD_TAG} .
+docker build --tag=udafinalproject${env.BUILD_TAG} .
 
 # Step 2: 
 # List docker images
