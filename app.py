b25871ee2891a79a930007349baf6d988da548ec
Manuel Eiras Casal <manuel.eirascasal@icloud.com>
2020-10-28 22:30:01 +0000

    adding ansible plays

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World, my name is Manuel Eiras Casal"
+    return "Hello World test, my name is Manuel Eiras Casal"
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0')
